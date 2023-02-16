888e314540e5666905518f8106a39557fd9ebc2a
Faris Hadzisadikovic <hadzisadikovicfaris@gmail.com>
2023-02-16 13:54:59 -0500

    Stage 1 (almost done), missing the part to check if it is a file

--- karelConverterStage1.py
+++ karelConverterStage1.py
@@ -32,44 +32,65 @@ import binascii
 import sys
 import os
 
-hexdata = []
+hexdata = []        
+sys.argv = sys.argv[1:] 
+counter = 0
 
-def isJarel():
-    if byteValue == "0e32ee4672937cf8":  
-        #this is the part where it starts writing a new file with the converted string
-        print("This is a Jarel world file.\n")
-        convertedStr = hexdata[0:len(hexdata)-64] + "c07500c54fec2e52" + hexdata[-48:]
-        print(convertedStr + "\n")
-        print("This is now a Karel world file.\n")
-
-def isKarel():
-    if byteValue == "c07500c54fec2e52":
-        print("This is a Karel world file.\n")
-        convertedStr = hexdata[0:len(hexdata)-64] + "0e32ee4672937cf8" + hexdata[-48:]
-        print(convertedStr + "\n")
-        print("This is now a Jarel world file.\n")
-
-def notKarelOrJarel():
-    if (byteValue != "c07500c54fec2e52") and (byteValue != "0e32ee4672937cf8"):
-        print("This is not a Karel world file nor a Jarel world file.\n")
-        
-sys.argv = sys.argv[1:]  
 if len(sys.argv) == 0:
       sys.exit("You need to put a file or directory.")
+
 for path in sys.argv:
     #if os.path.isdir(path) or os.path.isfile(path):
     with open(path, 'rb') as f:
-        hexdata = f.read().hex()
-
-        print(hexdata, "\n")
-
+        hexdata = f.read().hex() #this will read all the hexdata
+        
+        binStr = binascii.unhexlify(hexdata)
+        
+        fileLocation = sys.argv[counter]
+        
+        #print(fileLocation, "\n") #this prints out the name of the argument that is being looked at at the time
+        #print(binStr, "\n")
+            
         """
         This will print out the 8 bytes that we can compare to see
         if it is a Karel or a Jarel world file
         """
         byteValue = hexdata[-64:-48]
-        print(byteValue, "\n")
-
-        isJarel()
-        isKarel()
-        notKarelOrJarel()
+        #print(byteValue, "\n")
+        #print(binascii.unhexlify(byteValue), "\n")
+        
+        base = os.path.splitext(fileLocation)[0]
+        #print(base)
+        
+        if byteValue == "c07500c54fec2e52": #check if it is a Karel world file
+            #print("This is a Karel world file.\n")
+            convertedString = hexdata[0:len(hexdata)-64] + "0e32ee4672937cf8" + hexdata[-48:]
+            #print(binascii.unhexlify(convertedString), "\n")
+            #print(convertedStr + "\n")
+            #print("This is now a Jarel world file.\n")
+            os.rename(fileLocation, base + '.kw')
+            
+        elif byteValue == "0e32ee4672937cf8": #check if it is a Jarel world file
+            #this is the part where it starts writing a new file with the converted string
+            #print("This is a Jarel world file.\n")
+            convertedString = hexdata[0:len(hexdata)-64] + "c07500c54fec2e52" + hexdata[-48:]
+            #print(binascii.unhexlify(convertedString), "\n")
+            #print(convertedStr + "\n")
+            #print("This is now a Karel world file.\n")
+            os.rename(fileLocation, base + '.jw')
+        else:
+            print("This is not a Karel world file nor a Jarel world file.\n")
+            
+        convert = binascii.unhexlify(convertedString)
+        
+        """
+        If it is a Karel world file make the new converted file with the extension '.jw'
+        """
+        if byteValue == "c07500c54fec2e52": 
+            with open(base + ".jw", 'wb') as f:
+                f.write(convert)
+        elif byteValue == "0e32ee4672937cf8":
+            with open(base + ".kw", 'wb') as f:
+                f.write(convert)
+                
+        counter += 1 #incrementation of fileName which means it is looking at eveery argument at the command line
