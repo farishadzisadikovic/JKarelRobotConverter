880a96a1b7a0605d58b594e9a1a88691b8fddc0a
Faris Hadzisadikovic <70404554+farishadzisadikovic@users.noreply.github.com>
2023-05-08 22:49:11 -0400

    Update converterGUI.py

--- converterGUI.py
+++ converterGUI.py
@@ -1,26 +1,7 @@
 """
 Faris Hadzisadikovic
 2/17/2023
-
-JKarelRobot Converter:
-
-This program is a GUI application written in Python using the Tkinter library. 
-It provides a graphical user interface to convert files between Jarel World files 
-and Karel World files, and vice versa. 
-
-The GUI has a label to show the selected file or directory, and four buttons:
-1. Select File(s) - opens a file dialog for selecting one or more files.
-2. Select Folder(s) - opens a folder dialog for selecting one or more folders.
-3. Remove Selected - removes the selected files or folders from the list.
-4. Clear - clearing the listbox
-5. Convert - performs the file conversion on the selected files and folders.
-
-The selected files and folders are displayed in a listbox. The listbox has a scrollbar, 
-which is displayed on the right-hand side. When the Convert button is pressed, 
-the program reads in each file, checks if it is a Jarel World file or a Karel World file, 
-and converts it to the other format. The converted file is saved with the same 
-filename but with a different extension. After the conversion, a message box is displayed to 
-inform the user that the conversion was successful.
+JKarelRobot Converter
 """
 
 import binascii
